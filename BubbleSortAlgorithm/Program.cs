f8badf605a533a93d357f0fd6d6a5b6886f815aa
sumonongit <sumu2007@gmail.com>
2019-04-13 17:24:57 +0600

    This is first commit

---
+++ BubbleSortAlgorithm/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSortAlgorithm
+{
+    public class BubbleSort
+    {
+        int[] numArray = new int[] { 1, 5, 3, 18, 30, 22, 37, 7, 11, 8, 12, 2, 50, 41, 15 };
+        int temp;
+
+        public int[] DoSort()
+        {
+            for (int i = 0; i < numArray.Count(); i++)
+            {
+                for (int j = 0; j < numArray.Count() - 1; j++)
+                {
+                    if (numArray[j] > numArray[j + 1])
+                    {
+                        //SWAPPING THE VALUES
+                        temp = numArray[j];
+                        numArray[j] = numArray[j + 1];
+                        numArray[j + 1] = temp;
+                    }
+                }
+            }
+
+            return numArray;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BubbleSort bs = new BubbleSort();
+            int[] sortArray = bs.DoSort();
+            Console.WriteLine("The sorted numbers are as follows:");
+            Console.WriteLine("==================================");
+            for (int i = 0; i < sortArray.Count(); i++)
+            {
+                Console.Write("{0} ", sortArray[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
